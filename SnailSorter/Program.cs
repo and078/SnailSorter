e5c7a8e43a414d59977a8aa7927f19b782e90b79
and078 <34898237+and078@users.noreply.github.com>
2022-06-14 08:45:08 +0300

    Add project files.

---
+++ SnailSorter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsApp;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr =
+            {
+                new []{ 1, 2, 3, 4, 5, 6, 7},
+                new []{ 8, 9, 10, 11, 12, 13, 14},
+                new []{ 15, 16, 17, 18, 19, 20, 21},
+                new []{ 22, 23, 24, 25, 26, 27, 28},
+                new []{ 29, 30, 31, 32, 33, 34, 35}
+            };
+            int[] result = SnailSolution.Snail(arr);
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
+
---
+++ SnailSorter/Snail.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsApp
+{
+    class SnailSolution
+    {
+
+        private static void TopToRight(int[][] matrix, int[] arr, int loopCounter, int x, int y, int resultCurrentIndex)
+        {
+            for (int i = 0; i < x - loopCounter * 2; i++)
+            {
+                arr[resultCurrentIndex + i] = matrix[loopCounter][loopCounter + i];
+            }
+        }
+        private static void RightToBottom(int[][] matrix, int[] arr, int loopCounter, int x, int y, int resultCurrentIndex)
+        {
+            for (int i = 0; i < y - 1 - loopCounter * 2; i++)
+            {
+                arr[resultCurrentIndex + i] = matrix[loopCounter + 1 + i][x - 1 - loopCounter];
+            }
+        }
+        private static void BottomToLeft(int[][] matrix, int[] arr, int loopCounter, int x, int y, int resultCurrentIndex)
+        {
+            for (int i = 0; i < x - 1 - loopCounter * 2; i++)
+            {
+                arr[resultCurrentIndex + i] = matrix[y - 1 - loopCounter][x - 2 - i - loopCounter];
+            }
+        }
+        private static void LeftToTop(int[][] matrix, int[] arr, int loopCounter, int x, int y, int resultCurrentIndex)
+        {
+            for (int i = 0; i < y - 2 - loopCounter * 2; i++)
+            {
+                arr[resultCurrentIndex + i] = matrix[y - 2 - loopCounter - i][loopCounter];
+            }
+        }
+        public static int[] Snail(int[][] matrix)
+        {
+            if (matrix.Length == 0 || matrix[0].Length == 0)
+            {
+                return new int[] { 0 };
+            } 
+
+            int x = matrix[0].Length;
+            int y = matrix.Length;
+            int[] result = new int[y * x];
+            int loopCounter = 0;
+            int resultCurrentIndex = 0;
+            
+            while (true)
+            {
+                if (resultCurrentIndex < x * y)
+                {
+                    TopToRight(matrix, result, loopCounter, x, y, resultCurrentIndex);
+                    resultCurrentIndex = resultCurrentIndex + x - loopCounter * 2;
+                }
+                else break;
+
+                if (resultCurrentIndex < x * y)
+                {
+                    RightToBottom(matrix, result, loopCounter, x, y, resultCurrentIndex);
+                    resultCurrentIndex = resultCurrentIndex + y - 1 - loopCounter * 2;
+                }
+                else break;
+
+                if (resultCurrentIndex < x * y)
+                {
+                    BottomToLeft(matrix, result, loopCounter, x, y, resultCurrentIndex);
+                    resultCurrentIndex = resultCurrentIndex + x - 1 - loopCounter * 2;
+                }
+                else break;
+
+                if (resultCurrentIndex < x * y)
+                {
+                    LeftToTop(matrix, result, loopCounter, x, y, resultCurrentIndex);
+                    resultCurrentIndex = resultCurrentIndex + y - 2 - loopCounter * 2;
+                }
+                else break;
+
+                loopCounter++;
+            }
+            return result;
+        }
+    }
+}
